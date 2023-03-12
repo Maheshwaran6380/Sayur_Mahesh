6ea8f84ef2f60c7fe40183cf77783e20f55fdc5e
Maheshwaran <maheswaran6380@gmail.com>
2023-03-12 19:37:12 +0530

    updated

--- List_Problems/cafe.py
+++ List_Problems/cafe.py
@@ -2,73 +2,103 @@
 stock in the evening, sales amount at the end of the day and profit for each item. You need to restock an item 
 if the supply reaches 20% of the stock. Print the 3 items with highest sales, and top 3 highest profit."""
 
-items = ["coffee", "cookies", "tea", "donuts", "icecreams"]
-itemPrice = [15, 10, 15, 20, 20]
-itemStocks = [100, 50, 100, 40, 30]
-refillitem = [100,50,100,40,30]
-itemSale = [0,0,0,0,0]
-itemProfit = [3, 2, 3, 5, 4]
-eachitemprofit = [0,0,0,0,0]
-
-
-def process(list1):
-        numberList1 = ["1","2","3","4","5","6","7","8","9"]
-        numberList2 = ["one","two","three","four","five","six","seven","eight","nine"]
-        quantity = 0
-        quantityList = [0,0,0,0,0]
-        for i in list1:
-                if i in numberList1:
-                        quantity = int(i)
-                elif i in numberList2:
-                        quantity = numberList2.index(i) + 1
-                elif i in items:
-                        indexitem = items.index(i)
-                        quantityList[indexitem] = quantity
-        return quantityList
-
-def refillstock():
-       for i in range(len(itemStocks)):
-            if itemStocks[i] == itemStocks[i]*0.2:
-                   itemStocks[i] = refillitem[i]
-
-def findmax(li):
-    return li.index(max(li))
-
-print("------MENU CARD------")
-for j in range(len(items)):
-        print(f"{items[j]} : {itemPrice[j]}rs")
-
+items = ["coffee", "tea", "cookies", "donuts", "bun"]
+itemPrice = [20, 20, 10, 15, 5]
+itemStock = [100, 100, 50, 60, 40]
+refillStock = [100, 100, 50, 60, 40]
+itemProfit = [5, 5, 3, 4, 2]
+soldItems = [0, 0, 0, 0, 0]
+profitItem = [0, 0, 0, 0, 0]
 customers = 1
-yourbill=0
+
+def processinput(inputList):
+    # function for give the proccessed input
+    numList1 = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+    numList2 = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+    quantity = 0
+    quantityList = [0, 0, 0, 0, 0]
+    for word in inputList:
+        if word in numList1:
+            quantity = int(word)
+        elif word in numList2:
+            quantity = numList2.index(word) + 1
+        elif word in items:
+            quantityList[items.index(word)] = quantity
+    return quantityList
+
+def billing(custIn):
+    bill = 0
+    for i in range(len(custIn)):
+        bill += custIn[i] * itemPrice[i]
+    return bill
+
+def updateStock(custIn):
+    # updating stock
+    for i in range(len(itemStock)):
+        itemStock[i] -= custIn[i]
+
+    # refilling stock if it reaches 20%
+    for i in range(len(itemStock)):
+        if itemStock[i] == itemStock[i] * 0.2:
+            itemStock[i] = refillStock[i]
+
+def updateSoldItems(custIn):
+    for i in range(len(soldItems)):
+        soldItems[i] += custIn[i]
+
+def updateProfit(custIn):
+    for i in range(len(itemProfit)):
+        profitItem[i] += custIn[i] * itemProfit[i]
+
+def printProfit(custIn,count):
+    print("High Profit Items : ")
+    list_profit=[]
+    temp = profitItem
+    for i in range(count):
+        maxx = temp.index(max(temp))
+        temp[maxx] = 0
+        list_profit.append(items[maxx])
+    print(list_profit)
+
+def printSold(custin,count):
+    print("High sold Items : ")
+    list_sold=[]
+    temp = soldItems
+    for i in range(count):
+        maxx = temp.index(max(temp))
+        temp[maxx] = 0
+        list_sold.append(items[maxx])
+    print(list_sold)
 
 while customers <= 5:
-    print(f"\n Customer no:{customers}")
-    customerInput = input("What would you like to order ? ").lower().split(" ")
-    customerInput = process(customerInput)
-    for i in range(len(itemStocks)):
-        itemStocks[i] -= customerInput[i]
-        itemSale[i]+=customerInput[i]
-    refillstock()
-    customers += 1
+    # begin loop
+    print("Customer no : " , customers)
+    print("----- MENU CARD -----")
+    for i in range(len(items)):
+        print(f"{items[i]} : {itemPrice[i]}")  # to print the menu
+    custIn = input("What would you like to order:").lower().split(" ")  # input of the customer
+    custIn = processinput(custIn)
+    print("Thanks for ordering ,  Enjoy your food")
+    print("Your bill is :", billing(custIn))
+    billing(custIn)
+    updateStock(custIn)
+    updateSoldItems(custIn)
+    updateProfit(custIn)
+    customers = customers + 1
+    # end loop
 
-print("\nITEMS SOLD...")
-for i in range(len(itemSale)):
-    if not itemSale[i]==0:
-        print(f"{items[i]} sold : {itemSale[i]}")
-    eachitemprofit[i] = itemSale[i] * itemProfit[i]     #profit calculation
+print("Item sold")
+for i in range(len(items)):
+    print(f"{items[i]} sold : {soldItems[i]}")
 
-print("\nAvailable stock...")
-for i in range(len(itemSale)):
-        print(f"{items[i]} sold : {itemStocks[i]}")
+print("Available stock")
+for i in range(len(items)):
+    print(f"{items[i]} available  : {itemStock[i]}")
 
-print("\nTop 3 highest profit : ")
-for i in range(3):
-    a = findmax(eachitemprofit)
-    print(items[a])
-    eachitemprofit[a]=0
+count = 0
+for i in soldItems:
+    if not i == 0:
+        count = count +1
 
-print("\nTop 3 highest sales : ")
-for i in range(3):
-    a = findmax(itemSale)
-    print(items[a])
-    itemSale[a]=0
+printProfit(custIn,count)
+printSold(custIn,count)
